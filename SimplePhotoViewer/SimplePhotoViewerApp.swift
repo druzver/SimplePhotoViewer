28d16af88b6407c022f927a438dfd9ca9afe4bc6
Vitaly Yeremenko <i.have.mail.in@gmail.com>
2023-06-15 11:27:10 +0300

    initial commit

---
+++ SimplePhotoViewer/SimplePhotoViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimplePhotoViewerApp.swift
+//  SimplePhotoViewer
+//
+//  Created by Vitaly on 15.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct SimplePhotoViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
